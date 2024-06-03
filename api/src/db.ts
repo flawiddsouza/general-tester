8bd7089dd55e98b0f96eb5bc2ecd05620fdfeac0
Flawid DSouza <me@flawiddsouza.com>
2024-06-03 13:19:15 +0530

    api: adds matching apis for all tables

--- api/src/db.ts
+++ api/src/db.ts
@@ -15,12 +15,24 @@ import { eq } from 'drizzle-orm'
 const sqlite = new Database('./data/store.db')
 const db = drizzle(sqlite, { schema: { workflows, environments, nodes, edges } })
 
-export async function createWorkflow(workflow: workflow) {
-    return await db.insert(workflows).values(workflow)
-}
-
 export async function getWorkflows() {
     return await db.select(workflow).from(workflows)
+}
+
+export async function getWorkflow(id: workflow['id']) {
+    const nodesData = await db.select(node).from(nodes).where(eq(nodes.workflow_id, id))
+    const edgesData = await db.select(edge).from(edges).where(eq(edges.workflow_id, id))
+    const environmentsData = await db.select(environment).from(environments).where(eq(environments.workflow_id, id))
+
+    return {
+        environments: environmentsData,
+        nodes: nodesData,
+        edges: edgesData,
+    }
+}
+
+export async function createWorkflow(workflow: workflow) {
+    return await db.insert(workflows).values(workflow)
 }
 
 export async function updateWorkflow(id: workflow['id'], update: Partial<workflow>) {
@@ -36,10 +48,6 @@ export async function createEnvironment(environment: environment) {
     return await db.insert(environments).values(environment)
 }
 
-export async function getEnvironments(workflowId: environment['workflow_id']) {
-    return await db.select(environment).from(environments).where(eq(environments.workflow_id, workflowId))
-}
-
 export async function updateEnvironment(id: environment['id'], update: Partial<environment>) {
     return await db.update(environments).set(update)
        .where(eq(environments.id, id))
@@ -53,10 +61,6 @@ export async function createNode(node: node) {
     return await db.insert(nodes).values(node)
 }
 
-export async function getNodes(workflowId: node['workflow_id']) {
-    return await db.select(node).from(nodes).where(eq(nodes.workflow_id, workflowId))
-}
-
 export async function updateNode(id: node['id'], update: Partial<node>) {
     return await db.update(nodes).set(update)
        .where(eq(nodes.id, id))
@@ -68,10 +72,6 @@ export async function deleteNode(id: node['id']) {
 
 export async function createEdge(edge: edge) {
     return await db.insert(edges).values(edge)
-}
-
-export async function getEdges(workflowId: edge['workflow_id']) {
-    return await db.select(edge).from(edges).where(eq(edges.workflow_id, workflowId))
 }
 
 export async function updateEdge(id: edge['id'], update: Partial<edge>) {
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,82 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
+import { workflow, environment, node, edge } from './schema'
+import {
+    getWorkflows,
+    getWorkflow,
+    createWorkflow,
+    updateWorkflow,
+    deleteWorkflow,
+    createEnvironment,
+    updateEnvironment,
+    deleteEnvironment,
+    createNode,
+    updateNode,
+    deleteNode,
+    createEdge,
+    updateEdge,
+    deleteEdge
+} from './db'
 
 const app = new Elysia()
     .get('/', () => 'Hello Elysia')
+    .get('/workflows', () => {
+        return getWorkflows()
+    })
+    .get('/workflow/:id', ({ params }) => {
+        return getWorkflow(params.id)
+    })
+    .post('/workflow', ({ body }) =>{
+        return createWorkflow(body)
+    }, {
+        body: workflow
+    })
+    .put('/workflow/:id', ({ params, body }) => {
+        return updateWorkflow(params.id, body)
+    }, {
+        body: t.Partial(workflow)
+    })
+    .delete('/workflow/:id', ({ params }) => {
+        return deleteWorkflow(params.id)
+    })
+    .post('/environment', ({ body }) =>{
+        return createEnvironment(body)
+    }, {
+        body: environment
+    })
+    .put('/environment/:id', ({ params, body }) => {
+        return updateEnvironment(params.id, body)
+    }, {
+        body: t.Partial(environment)
+    })
+    .delete('/environment/:id', ({ params }) => {
+        return deleteEnvironment(params.id)
+    })
+    .post('/node', ({ body }) =>{
+        return createNode(body)
+    }, {
+        body: node
+    })
+    .put('/node/:id', ({ params, body }) => {
+        return updateNode(params.id, body)
+    }, {
+        body: t.Partial(node)
+    })
+    .delete('/node/:id', ({ params }) => {
+        return deleteNode(params.id)
+    })
+    .post('/edge', ({ body }) =>{
+        return createEdge(body)
+    }, {
+        body: edge
+    })
+    .put('/edge/:id', ({ params, body }) => {
+        return updateEdge(params.id, body)
+    }, {
+        body: t.Partial(edge)
+    })
+    .delete('/edge/:id', ({ params }) => {
+        return deleteEdge(params.id)
+    })
     .listen(3000)
 
 console.log(
