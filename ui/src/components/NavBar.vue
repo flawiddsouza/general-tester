1c236fd0035c6ac0a71a1ca1f2a61cbf06b36154
Flawid DSouza <me@flawiddsouza.com>
2024-06-12 15:10:27 +0530

    ui: navbar > button to export active workflow

--- ui/src/components/NavBar.vue
+++ ui/src/components/NavBar.vue
@@ -4,7 +4,8 @@
             General Tester <template v-if="store.activeWorkflow">> {{ store.activeWorkflow.name }}</template>
         </div>
         <div>
-            <button @click="runWorkflow(store.activeWorkflow as Workflow)" :disabled="!store.activeWorkflow">Run workflow</button>
+            <button @click="exportWorkflow(store.activeWorkflow as Workflow)" :disabled="!store.activeWorkflow">Export workflow</button>
+            <button @click="runWorkflow(store.activeWorkflow as Workflow)" :disabled="!store.activeWorkflow" class="ml-1">Run workflow</button>
         </div>
     </nav>
 </template>
@@ -13,6 +14,21 @@
 import { useStore } from '@/store'
 
 const store = useStore()
+
+async function exportWorkflow(workflow: Workflow) {
+    const blob = new Blob([JSON.stringify({
+        workflow,
+        environments: store.environments,
+        nodes: store.nodes,
+        edges: store.edges,
+    }, null, 4)], { type: 'application/json' })
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = url
+    a.download = `${workflow.name}.json`
+    a.click()
+    URL.revokeObjectURL(url)
+}
 
 async function runWorkflow(workflow: Workflow) {
     store.workflowLogs = []
